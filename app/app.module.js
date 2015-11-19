0b93152d2afeae0c7a59ce6666506b7cfb75db1b
Tomasz Bialecki <tomasz.bialecki@capgemini.com>
2015-11-19 13:22:27 +0100

    form implemented

--- app/app.module.js
+++ app/app.module.js
@@ -1,1 +1,29 @@
-angular.module('app', ['app.main']);
+angular.module('app', ['app.main'])
+    .controller('SampleCntl', function () {
+        var sample = this;
+
+        sample.user = {
+
+        };
+
+
+        sample.isNameValid = function () {
+            return sample.myForm.userName.$invalid && sample.myForm.userName.$dirty
+        };
+
+        sample.submit = function () {
+            if (sample.myForm.$valid) {
+                console.log('valid');
+            } else {
+                console.log('invalid');
+            }
+        };
+        sample.resetForm = function () {
+            sample.user = {};
+            sample.myForm.$setPristine();
+            sample.myForm.$setUntouched();
+        };
+        sample.submitActive = function () {
+            return sample.myForm.$valid && sample.user.password === sample.password2;
+        }
+    });
